04181223512c0b3d861328323c10fc1a9ce69361
DarkDav <dadasovdavid@gmail.com>
2022-04-21 13:53:09 +0300

    Uncheck

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,10 +1,13 @@
 package koschei.config;
 
+import koschei.models.Duck5;
 import koschei.models.Island2;
+import koschei.models.Rabbit4;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
@@ -14,4 +17,8 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+    @Bean
+    public static Rabbit4 getRabbit(Duck5 duck5){ return new Rabbit4(duck5);}
+
+
 }
